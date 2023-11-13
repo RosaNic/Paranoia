7f8a5f841791deb05748d4c9a6ee21b42e51ba60
Nicole <rosanik3103@gmail.com>
2023-11-13 17:33:46 -0300

    primeira atualização

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿
+
+Console.Clear();
+
+Console.WriteLine
+    ("Qual seu nome jogador?");
+string nome = Console.ReadLine()!;
+void rgb(int v1, int v2, int v3)
+{
+    throw nome();
+}
+rgb(177, 201, 0);
+
+
+
+Console.WriteLine
+    ($"Você está feliz, {nome}?");
+Console.ReadKey();
+Console.WriteLine();
+Console.WriteLine
+    ("Não importa!\nÓbvio que está, este é o Complexo Platina, a felicidade é obrigatória!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*class Entrada
+{
+    public string? Texto;
+    public List<int> Alternativas = new();
+}
+
+string nome.textcolor(GREEN);
+
+void textcolor(object gREEN)
+{
+    throw new NotImplementedException();
+}
+
+
+
+*/
